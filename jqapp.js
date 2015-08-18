a12dabd8fb781e3131104ea16f1cc9340748d487
RichardCharman <richardcharman@live.com>
2015-08-18 15:21:24 +0000

    Added jquery code for all functions

---
+++ jqapp.js
@@ -0,0 +1,23 @@
+  var therm = new Thermostat(); 
+   function showTherm() {
+     $("#temp").text(therm.temp);
+     $("#temp").css("color", therm.colour());
+   };
+$(document).ready(function() {
+        showTherm();
+      $("#up").click(function() {
+        therm.up();
+        showTherm();
+      });
+      $("#down").click(function() {
+        therm.down();
+        showTherm();
+      });
+      $("#reset").click(function() {
+        therm.restart();
+        showTherm();
+      });
+      $("#psmbox").click(function() {
+        therm.psmSwitch();
+      });
+    });
