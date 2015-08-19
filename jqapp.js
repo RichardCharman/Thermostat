acf3e5ae2e2195e8eeaf4832c00cecee23916b18
RichardCharman <richardcharman@live.com>
2015-08-19 18:57:06 +0000

    Added functional Open Weather API, and additional CSS

--- jqapp.js
+++ jqapp.js
@@ -1,8 +1,18 @@
-  var therm = new Thermostat(); 
-   function showTherm() {
-     $("#temp").text(therm.temp);
-     $("#temp").css("color", therm.colour());    
-   };
+var therm = new Thermostat(); 
+function showTherm() {
+   $("#temp").text(therm.temp);
+   $("#temp").css("color", therm.colour());    
+};
+
+var weatherAPI = function(cityName) {
+     $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=metric&APPID=b0aea1fc7fa650c8be747007e4f8e609", function(cityJSON) {
+       $('#name').html(cityJSON.name);
+       $('#temperature').html(Math.round(cityJSON.main.temp));
+       $('#degrees').removeClass();
+       $('#degrees').addClass("degrees_show");
+    });
+  };
+
 $(document).ready(function() {
         showTherm();
       $("#up").click(function() {
@@ -19,5 +29,10 @@ $(document).ready(function() {
       });
       $("#psmbox").click(function() {
         therm.psmSwitch();
+      });
+      $('#get_weather').click(function() {
+        var city = $('#city_choice').val();
+        weatherAPI(city);
+        $('#city_choice').val('');
       });
     });
